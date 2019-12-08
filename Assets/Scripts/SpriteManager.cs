51db8fcbd0f50713eb6b69c2c52cce131177c979
Ren H <kerensahughes@gmail.com>
2019-12-07 20:01:10 -0500

    Started the Sprite Manager script

--- Assets/Scripts/SpriteManager.cs
+++ Assets/Scripts/SpriteManager.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ink.Runtime;
 
-public class SpriteManager : MonoBehaviour
-{
+public class SpriteManager : MonoBehaviour { 
+
+    public GameObject player1;
+    public GameObject player2;
+
+    public Story rightStory;
+    public Story LeftStory; 
+
+    public Sprite Mac; 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rightStory = player2.GetComponent<BASEInkIntegration>().GetStory();
+        LeftStory = player1.GetComponent<BASEInkIntegration>().GetStory(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void CreateContentView(string text)
+    {
+        var storyText = Instantiate(textPrefab);
+        var storyImage = Instantiate(imagePrefab);
+        storyText.text = text;
+        storyText.transform.SetParent(canvas.transform, false);
+        storyImage.transform.SetParent(canvas.transform, false);
+
+
+        storyText.text = text;
     }
 }
