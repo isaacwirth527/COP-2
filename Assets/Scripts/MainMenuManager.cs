cefe46bc8d2987eb592f23f004244ddb200a2b22
isaacwirth527 <49846299+isaacwirth527@users.noreply.github.com>
2019-12-11 22:59:55 -0500

    working on integrating menu with game

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -3,32 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
 using UnityEngine.UI; 
+using TMPro;
 
 public class MainMenuManager : MonoBehaviour
 {
 
     public Text TitleText; //text for title
-
-    public Text StartButton;
-
-    public Text DescText; 
+    public Text TitleText2;
+    public Button StartButton;
+    public Button StartButton2;
+    public TextMeshProUGUI DescText; 
+    public TextMeshProUGUI DescText2;
     // Start is called before the first frame update
     void Start()
     {
+        DescText.text = "";
+        DescText2.text = "";
+        StartButton.Select();
+        StartButton2.Select();
         //StartButton.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            TitleText.text = "";
-            StartButton.text = "";
-            DescText.text = "Welcome to my twisted mind!";
-
-        }
         
+    }
+
+    public void OnSwitch()
+    {
+      TitleText.text = "";
+      TitleText2.text = "";
+      StartButton.GetComponentInChildren<Text>().text = "";
+      StartButton2.GetComponentInChildren<Text>().text = "";
+      DescText.text = "Welcome to my twisted mind!";
+      DescText2.text = "Welcome to my twisted mind!";
+
     }
 
  
